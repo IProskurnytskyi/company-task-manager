3a5f66c3e9cb77c63c1ab93e90d137caf1ee59b1
Ivan Proskurnytskyi <prsky.ivan@gmail.com>
2023-09-18 18:02:32 +0200

    Creating Home Page for Task Manager

--- task/views.py
+++ task/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from task.models import TaskType, Task, Position, Worker
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    task_types = TaskType.objects.count()
+    tasks = Task.objects.count()
+    positions = Position.objects.count()
+    workers = Worker.objects.count()
+
+    context = {
+        "task_types": task_types,
+        "tasks": tasks,
+        "positions": positions,
+        "workers": workers,
+    }
+
+    return render(request, "task/index.html", context)
