b05600cec510fb041f7f24dddc6679b7cdd8bec6
Ivan Proskurnytskyi <prsky.ivan@gmail.com>
2023-09-18 15:18:17 +0200

    Add ordering & string representation into models.py

--- task/models.py
+++ task/models.py
@@ -7,6 +7,9 @@ from task_manager import settings
 class TaskType(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     PRIORITY_CHOICES = [
@@ -30,12 +33,27 @@ class Task(models.Model):
         settings.AUTH_USER_MODEL, related_name="tasks"
     )
 
+    class Meta:
+        ordering = ["deadline"]
+
+    def __str__(self):
+        return self.name
+
 
 class Worker(AbstractUser):
     position = models.ForeignKey(
         "Position", on_delete=models.CASCADE, related_name="workers"
     )
 
+    class Meta:
+        ordering = ["position"]
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
 
 class Position(models.Model):
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
