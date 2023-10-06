1012a3539a0914160f17c23a2dc7fe1b1b2335fe
Ivan Proskurnytskyi <prsky.ivan@gmail.com>
2023-10-06 15:51:58 +0200

    Try to fix bug with allowed hosts

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "https://task-manager-wam2.onrender.com"]
+ALLOWED_HOSTS = ["task-manager-wam2.onrender.com", "127.0.0.1"]
 
 # Application definition
 
